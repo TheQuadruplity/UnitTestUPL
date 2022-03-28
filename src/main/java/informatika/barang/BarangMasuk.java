03c237522ffe57b1cbb265772e173e6f1ce1b34f
TheQuadruplity <thev.vandicco@gmail.com>
2022-03-28 21:27:50 +0700

    object barang masuk

--- src/main/java/informatika/barang/BarangMasuk.java
+++ src/main/java/informatika/barang/BarangMasuk.java
@@ -1,14 +1,19 @@
-/**
- * 
- */
 package informatika.barang;
 
-/**
- * @author Aveneraa
- *
- */
+import java.util.Scanner;
+
 public class BarangMasuk {
-	public String init(){
-        return "Data barang berhasil diinputkan!";
+    private Scanner S;
+
+    public BarangMasuk(){
+        S = new Scanner(System.in);
+    }
+
+    public void inputBarang(){
+        String nama = this.S.nextLine();
+        int stok = this.S.nextInt();
+        String jenis = this.S.nextLine();
+        int harga = this.S.nextInt();
+        Barang.addBarang(nama, String.valueOf(stok), jenis, String.valueOf(harga));
     }
 }
