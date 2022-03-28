0967155a58f8d31d8bb24b0172a1b9e313002d07
TheQuadruplity <thev.vandicco@gmail.com>
2022-03-28 20:31:16 +0700

    Barang test array + object

--- src/main/java/informatika/barang/Barang.java
+++ src/main/java/informatika/barang/Barang.java
@@ -1,7 +1,59 @@
 package informatika.barang;
 
 public class Barang {
-	public String testBarang(){
-        return "BRRRIIIHHH";
+    static final String[][] arrayBarang = {
+        {"Buku Gambar", "3", "Buku", "5000"},
+        {"Pensil", "4", "ATK", "2500"},
+        {"Baju", "2", "Busana", "75000"},
+        {"Sepeda", "2", "Kendaraan", "1000000"}
+    };
+
+    private int id;
+    private String nama;
+    private int stok;
+    private String jenis;
+    private int harga;
+
+    static int length(){
+        return Barang.arrayBarang.length;
+    }
+
+    public Barang(int id){
+        this.id = id;
+        this.nama = Barang.arrayBarang[id][0];
+        this.stok = Integer.parseInt(Barang.arrayBarang[id][1]);
+        this.jenis = Barang.arrayBarang[id][2];
+        this.harga = Integer.parseInt(Barang.arrayBarang[id][3]);
+    }
+
+    public int getId() {
+        return id;
+    }
+    public int getHarga() {
+        return harga;
+    }
+    public String getJenis() {
+        return jenis;
+    }
+    public String getNama() {
+        return nama;
+    }
+    public int getStok() {
+        return stok;
+    }
+    public void setHarga(int harga) {
+        this.harga = harga;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public void setJenis(String jenis) {
+        this.jenis = jenis;
+    }
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+    public void setStok(int stok) {
+        this.stok = stok;
     }
 }
